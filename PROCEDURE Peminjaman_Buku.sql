2b52f81286fad2fa10b6139a0dd1c2b70d6d00a5
aqsalrafsss <ichall.jakarta@gmail.com>
2024-03-07 15:49:28 +0700

    Create PROCEDURE Peminjaman_Buku.sql

---
+++ PROCEDURE Peminjaman_Buku.sql
@@ -0,0 +1,20 @@
+PROCEDURE Peminjaman_Buku
+    INPUT nama_peminjam
+    INPUT judul_buku
+    IF buku_available(judul_buku) THEN
+        UPDATE status_buku(judul_buku) TO "Dipinjam"
+        PRINT "Buku", judul_buku, "telah berhasil dipinjam oleh", nama_peminjam
+    ELSE
+        PRINT "Maaf, buku", judul_buku, "sedang tidak tersedia untuk dipinjam"
+    END IF
+END PROCEDURE
+
+PROCEDURE Pengembalian_Buku
+    INPUT judul_buku
+    IF buku_dipinjam(judul_buku) THEN
+        UPDATE status_buku(judul_buku) TO "Tersedia"
+        PRINT "Buku", judul_buku, "telah berhasil dikembalikan"
+    ELSE
+        PRINT "Maaf, buku", judul_buku, "belum dipinjam"
+    END IF
+END PROCEDURE
